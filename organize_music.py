0268ccff9197c35f19e8da7603d2f75c1ee9418d
John Tibbetts <john.tibbetts@kinexis.com>
2017-02-22 15:33:03 -0800

    version 2

--- organize_music.py
+++ organize_music.py
@@ -27,7 +27,7 @@ all_dirs = []
 target_music_folder = None
 all_track_info = None
 track_location_dict = {}
-
+all_new_files = []
 
 def create_loid_segment(s):
     if s is None:
@@ -35,14 +35,8 @@ def create_loid_segment(s):
     return str(s).replace(" ", "")
 
 def create_playlist_dirname(playlist_name):
-    if playlist_name.endswith("::"):
-        playlist_name = playlist_name[0:-2]
     playlist_dirname = playlist_name.replace('::', '/')
     return re.sub(' +', '_', playlist_dirname)
-
-def normalize_str(ustr):
-    s = unicode_to_utf8(ustr)
-    return re.sub( '\s+', ' ', s).strip()
 
 def get_all_track_info(itlib):
     all_track_info = {}
@@ -53,8 +47,6 @@ def get_all_track_info(itlib):
 
 def get_original_files(target_folder_path):
     original_files = []
-    if "Elgar" in target_folder_path:
-        pass
     for root, dirs, files in os.walk(target_folder_path):
         # only immediate subdirectory
         if root == target_folder_path:
@@ -86,7 +78,7 @@ def mkdir_p(path):
 
 def normalize_track_name(name):
     if isinstance(name, unicode):
-        name = normalize_str(name)
+        name = unicode_to_ascii(name)
     name_with_blanks = name
     name = re.sub('\s+', '_', name)
     name = re.sub('\s*-\s*', '_', name)     # intervening ' - '
@@ -97,6 +89,48 @@ def normalize_track_name(name):
     name = re.sub('"', '_', name)
     name = re.sub('_+', '_', name)
     return (name, name_with_blanks)
+
+def parse_playlist_name(playlist_name):
+    prefix = ''
+    zones = playlist_name.split(':')
+    if len(zones) <= 1:
+        pass
+    else:
+        if "folder" in playlist_name.lower():
+            pass
+        prefix = parse_prefix(zones[0].strip().lower())
+        if prefix != '':
+            if len(zones) >= 2:
+                playlist_name = ':'.join(zones[1:])
+        else:
+            prefix = ''
+    playlist_name = playlist_name.strip()
+    display_playlist_name = unicode_to_utf8(playlist_name.replace('/', ' ').strip())
+    dir_playlist_name = unicode_to_ascii(playlist_name.replace(' ', '_')).strip()
+    return [prefix, display_playlist_name, dir_playlist_name]
+
+def parse_prefix(prefix):
+    result = ''
+    re_obj = re.match(r'^(\w+)(\W*)(\w+)?$', prefix)
+    if re_obj != None:
+        # two groups in regex
+        str1 = parse_token(re_obj, 0)
+        if str1 != '':
+            result = str1
+            str2 = parse_token(re_obj, 2)
+            if str2 != '':
+                result += ' ' + str2
+    return result
+
+def parse_token(re_obj, token_idx):
+    result = ''
+    check_value = re_obj.groups()[token_idx]
+    if check_value != None:
+        if 'folder'.startswith(check_value):
+            result = 'folder'
+        elif 'playlist'.startswith(check_value):
+            result = 'playlist'
+    return result
 
 def process_track_list(is_folder_not_playlist, track_list, m3u_entries, playlist_name):
     global target_music_folder,  all_track_info, track_location_dict
@@ -113,8 +147,6 @@ def process_track_list(is_folder_not_playlist, track_list, m3u_entries, playlist
 
         (track_name, track_name_display) = normalize_track_name(track.name)
         if is_folder_not_playlist:
-            if "Elgar" in track_name:
-                pass
             source_location = '/' + track.location
             file_name, file_ext = os.path.splitext(source_location)
             playlist_dirname = create_playlist_dirname(playlist_name)
@@ -127,8 +159,6 @@ def process_track_list(is_folder_not_playlist, track_list, m3u_entries, playlist
             track_location_dict[track_loid] = relative_path
         else:
             in_folder_name = next(iter(track_info['in_folders']), None)
-            if "Elgar" in track_name:
-                pass
             if in_folder_name == None:
                 source_location = '/' + track.location
                 file_name, file_ext = os.path.splitext(source_location)
@@ -146,10 +176,8 @@ def process_track_list(is_folder_not_playlist, track_list, m3u_entries, playlist
                 source_location = '/' + track.location
                 file_name, file_ext = os.path.splitext(source_location)
                 playlist_dirname = create_playlist_dirname(in_folder_name)
-                relative_path = playlist_dirname + '/' + ("%03d" % counter) + '-' + track.name + file_ext
+                relative_path = playlist_dirname + '/' + ("%03d" % counter) + '-' + track_name + file_ext
                 absolute_path = target_music_folder + '/' + relative_path
-                all_files.append(absolute_path)
-
 
         length_in_seconds = int(math.ceil(track.length / 1000.0))
         m3u_entries.append([length_in_seconds, track_name_display, relative_path])
@@ -158,19 +186,17 @@ def process_track_list(is_folder_not_playlist, track_list, m3u_entries, playlist
 
     return all_files
 
-def remove_old_files(target_folder_path, original_files, new_files):
-    # remove files in folder that aren't in source (a la rsync --delete)
-    if "Elgar" in target_folder_path:
-        pass
-    for file in original_files:
-        if not file in new_files:
-            os.remove(file)
-            print "Remove file " + file
-
 def remove_prefix(text, prefix):
     if text.startswith(prefix):
         return text[len(prefix):]
     return text
+
+def unicode_to_ascii(ustr):
+    if isinstance(ustr, unicode):
+        result = unicodedata.normalize('NFKD', ustr).encode('ascii', 'ignore')
+    else:
+        result = ustr
+    return result
 
 def unicode_to_utf8(ustr):
     if isinstance(ustr, unicode):
@@ -194,9 +220,8 @@ def write_m3u(path, entries):
             m3u_file.write(outline.encode('UTF-8'))
         m3u_file.close()
 
-
 def main(argv):
-    global target_music_folder,  all_track_info, track_location_dict
+    global target_music_folder,  all_track_info, track_location_dict, all_new_files
 
     usage_string = "organize_music.py -h <iTuneLibrary> <localMusicFolder>"
 
@@ -238,78 +263,67 @@ def main(argv):
 
     # Folder creation
     print "Create folders"
-    for playlist_name in itlib.getPlaylistNames():
-        playlist_name = playlist_name.strip()
-        # ignore playlists for the moment
-        if playlist_name.startswith('Playlist::'):
-            continue
+    for raw_playlist_name in itlib.getPlaylistNames():
+        (discriminator, display_playlist_name, dir_playlist_name) = parse_playlist_name(raw_playlist_name.strip())
 
-        if "::" in playlist_name:
-            display_playlist_name = unicode_to_utf8(playlist_name.replace('::', ' ').strip())
-
+        if 'folder' in discriminator:
             m3u_entries = []
 
-            playlist_dirname = create_playlist_dirname(playlist_name)
-            full_dirname = target_music_folder + '/' + playlist_dirname
+            full_dirname = target_music_folder + '/' + dir_playlist_name
             mkdir_p(full_dirname)
-            all_dirs.append(unicode_to_utf8(playlist_dirname))
+            all_dirs.append(unicode_to_utf8(dir_playlist_name))
 
             original_file_list = get_original_files(full_dirname)
 
             print "  Folder: " + display_playlist_name
 
-            track_list = itlib.getPlaylist(playlist_name).tracks
+            track_list = itlib.getPlaylist(raw_playlist_name).tracks
 
-            new_file_list = process_track_list(True, track_list, m3u_entries, playlist_name)
+            new_file_list = process_track_list(True, track_list, m3u_entries, dir_playlist_name)
 
-            remove_old_files(full_dirname, original_file_list, new_file_list)
+            all_new_files.extend(new_file_list)
 
-            # write out M3U files
-            local_m3u_path = target_music_folder + '/' + display_playlist_name + '.m3u'
-            write_m3u(local_m3u_path, m3u_entries)
-
-    # traverse local music folder and delete folders that are no longer used
-    remove_dirs = []
-    for root, dirs, files in os.walk(target_music_folder):
-        if root == target_music_folder:
-            continue
-        hit = False
-        for local_dir in all_dirs:
-            local_full_dir = target_music_folder + '/' + local_dir
-            if local_full_dir.startswith(unicode_to_utf8(root)):
-                hit = True
-                break
-        if not hit:
-            if not root.endswith("/Unassigned"):
-                remove_dirs.append(root)
-    for remove_dir in remove_dirs:
-        print ("removing " + remove_dir)
-        shutil.rmtree(remove_dir)
-
+            # write out M3U files..but only if explicitly requested
+            if 'playlist' in discriminator:
+                local_m3u_path = target_music_folder + '/' + display_playlist_name + '.m3u'
+                write_m3u(local_m3u_path, m3u_entries)
 
     print "Create playlists"
     unassigned_folder_name = target_music_folder + '/' + 'Unassigned'
     original_file_list = get_original_files(target_music_folder + '/' + 'Unassigned')
-    all_new_files = []
-    for playlist_name in itlib.getPlaylistNames():
-        if playlist_name.startswith('Playlist::'):
-            # trim Playlist prefix for display version
-            display_playlist_name = playlist_name[len('Playlist::'):].replace('::', ' ').strip()
-
+    for raw_playlist_name in itlib.getPlaylistNames():
+        (discriminator, display_playlist_name, dir_playlist_name) = parse_playlist_name(raw_playlist_name.strip())
+        if 'playlist' in discriminator:
             m3u_entries = []
 
             print "  Playlist: " + display_playlist_name
 
-            track_list = itlib.getPlaylist(playlist_name).tracks
+            track_list = itlib.getPlaylist(raw_playlist_name).tracks
 
-            new_file_list = process_track_list(False, track_list, m3u_entries, playlist_name)
+            new_file_list = process_track_list(False, track_list, m3u_entries, dir_playlist_name)
             all_new_files.extend(new_file_list)
 
             # write out M3U files
             local_m3u_path = target_music_folder + '/' + display_playlist_name + '.m3u'
             write_m3u(local_m3u_path, m3u_entries)
 
-    remove_old_files(unassigned_folder_name, original_file_list, all_new_files)
+    # delete obsolete files, then any empty dirs
+    for root, dirs, files in os.walk(target_music_folder):
+        if root == target_music_folder:
+            continue
+        for file in files:
+            if file == '.DS_Store':
+                continue
+            full_file = root + '/' + file
+            if not full_file in all_new_files:
+                os.remove(full_file)
+                print "Remove file " + full_file
+
+    for root, dirs, files in os.walk(target_music_folder):
+        if root == target_music_folder:
+            continue
+        if len(files) == 0 and len(dirs) == 0:
+            shutil.rmtree(root)
 
     print "Done"
     sys.exit(0)
